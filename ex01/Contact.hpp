71121dc255be14131916d4db94cd5176532dc847
ricvrdv <costagarcia.r95@gmail.com>
2025-07-04 19:42:06 +0100

    Started Ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 18:41:31 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/07/04 19:28:49 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+# include <string>
+
+class Contact
+{
+	std::string	index;
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+	public:
+		
+};
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 18:36:27 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/07/04 18:59:50 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook
+{
+	Contact contacts[8];
+	public:
+		PhoneBook(void);
+};
+
+#endif
