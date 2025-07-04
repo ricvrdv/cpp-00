aa01ddd3ab048f4d55050311bdc5ff62954e98ca
ricvrdv <costagarcia.r95@gmail.com>
2025-07-04 17:58:55 +0100

    Ex00 completed

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 17:19:40 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/07/04 17:55:07 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; j < std::strlen(argv[i]); j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
