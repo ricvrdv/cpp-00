16a6e0af568c530d4b6cd8f532ea8af2fb1dc5af
Ricardo Garcia <costagarcia.r95@gmail.com>
2025-07-09 00:39:13 +0100

    feat(ex02): define getters in Account.cpp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -3,11 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: applecore <applecore@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:23:41 by rjesus-d          #+#    #+#             */
-/*   Updated: 2025/07/08 18:35:47 by rjesus-d         ###   ########.fr       */
+/*   Updated: 2025/07/09 00:37:26 by applecore        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+int		Account::_nbAccounts = 0;
+int		Account::_totalAmount = 0;
+int		Account::_totalNbDeposits = 0;
+int		Account::_totalNbWithdrawals = 0;
+
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::Account( void )
+{
+    
+}
+
+int		Account::getNbAccounts( void )
+{
+    return (_nbAccounts);
+}
+
+int		Account::getTotalAmount( void )
+{
+    return (_totalAmount);
+}
+
+int		Account::getNbDeposits( void )
+{
+    return (_totalNbDeposits);
+}
+
+int		Account::getNbWithdrawals( void )
+{
+    return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::_displayTimestamp( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+    
+}
+
+Account::~Account( void )
+{
+
+}
